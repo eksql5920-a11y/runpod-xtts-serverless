8244863ba2c561af5e49aee8d59fdee3db9789c9
eksql5920-a11y <eksql5920@gmail.com>
2025-12-28 20:28:34 +0900

    Update audio_utils.py

--- serverless/audio_utils.py
+++ serverless/audio_utils.py
@@ -3,7 +3,7 @@ import io
 import soundfile as sf
 
 def wav_to_base64(wav, sample_rate=24000):
-    buffer = io.BytesIO()
-    sf.write(buffer, wav, sample_rate, format="WAV")
-    buffer.seek(0)
-    return base64.b64encode(buffer.read()).decode("utf-8")
+    buf = io.BytesIO()
+    sf.write(buf, wav, sample_rate, format="WAV")
+    buf.seek(0)
+    return base64.b64encode(buf.read()).decode("utf-8")
