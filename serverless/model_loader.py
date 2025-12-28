ca9d6365ad691a0e50ff0f0bc5fccd7084ba47db
eksql5920-a11y <eksql5920@gmail.com>
2025-12-28 20:24:47 +0900

    Update model loading to use available device

--- serverless/model_loader.py
+++ serverless/model_loader.py
@@ -1,10 +1,13 @@
 from TTS.api import TTS
+import torch
 
 def load_model():
-    # XTTS v2
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     tts = TTS(
         model_name="tts_models/multilingual/multi-dataset/xtts_v2",
         progress_bar=False,
-        gpu=True
+        gpu=device == "cuda"
     )
+
     return tts
